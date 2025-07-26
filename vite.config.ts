59a988c6dd485dfb07708e1b180a8b9f3049889c
yoye-zju <yoye_zju@163.com>
2025-07-26 14:08:05 +0800

    viteconfigts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/core-loss-fitter/',
+      base: '/test/',
       build: {
           cssCodeSplit: true,
           rollupOptions: {
