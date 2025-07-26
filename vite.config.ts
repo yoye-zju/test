e68f87812f8905981cba5de18dac815b5f6936ea
yoye-zju <yoye_zju@163.com>
2025-07-27 00:43:18 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '',
+      base: '/test/',
       build: {
           cssCodeSplit: true,
           rollupOptions: {
